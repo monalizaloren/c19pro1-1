96fd80c5307c39f6607ebaf4b6281ec32dccbdee
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-06 09:16:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,73 +5,11 @@ var ghost, ghostImg;
 var invisibleBlockGroup, invisibleBlock;
 var gameState = "play"
 
-function preload(){
-  towerImg = loadImage("tower.png");
-  doorImg = loadImage("door.png");
-  climberImg = loadImage("climber.png");
-  ghostImg = loadImage("ghost-standing.png");
-  spookySound = loadSound("spooky.wav");
-}
+//function preload
 
-function setup(){
-  createCanvas(600,600);
-  spookySound.loop();
-  tower = createSprite(300,300);
-  tower.addImage("tower",towerImg);
-  tower.velocityY = 1;
-  
-  doorsGroup = new Group();
-  climbersGroup = new Group();
-  invisibleBlockGroup = new Group();
-  
-  ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3;
-  ghost.addImage("ghost", ghostImg);
-}
+//function setup
 
-function draw(){
-  background(0);
-  if (gameState === "play") {
-    if(keyDown("left_arrow")){
-      ghost.x = ghost.x - 3;
-    }
-    
-    if(keyDown("right_arrow")){
-      ghost.x = ghost.x + 3;
-    }
-    
-    if(keyDown("space")){
-      ghost.velocityY = -10;
-    }
-    
-    ghost.velocityY = ghost.velocityY + 0.8
-    
-    if(tower.y > 400){
-      tower.y = 300
-    }
-    spawnDoors();
-
-    
-    //climbersGroup.collide(ghost);
-    if(climbersGroup.isTouching(ghost)){
-      ghost.velocityY = 0;
-    }
-    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
-      ghost.destroy();
-      gameState = "end"
-    }
-    
-    drawSprites();
-  }
-  
-  if (gameState === "end"){
-    stroke("yellow");
-    fill("yellow");
-    textSize(30);
-    text("Fim de Jogo", 230,250)
-  }
-
-}
+//function draw 
 
 function spawnDoors() {
   //escreva aqui o código para gerar as portas na torre
